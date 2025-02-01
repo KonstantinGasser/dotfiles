6830dfb2a91d1ca0757c6bfa2d4cd901f0663a81
KonstantinGasser <konstantin.gasser@me.com>
2025-02-01 19:16:23 +0100

    feat: nvim: adding latex support to config (latexmk + skim)

--- nvim/lua/konstantingasser/plugins/latex.lua
+++ nvim/lua/konstantingasser/plugins/latex.lua
@@ -7,6 +7,5 @@ return {
     vim.g.vimtex_view_method = 'skim'
     vim.g.vimtex_view_skim_sync = 1
     vim.g.vimtex_view_skim_activate = 1
-    -- vim.g.vimtex_view_general_options = '--synctex-forward @line:@col:@pdf'
   end,
 }
